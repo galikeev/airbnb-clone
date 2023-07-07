d57a3ff61da31503971120b99ab28ac37ecd12f8
Ramil Galikeev <galikeevramil95@gmail.com>
2023-07-07 17:55:39 +0500

    login user

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "@/app/components/modals/LoginModal";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 import "./globals.css";
 
 const inter = Nunito({ subsets: ["latin"] });
@@ -12,18 +14,20 @@ export const metadata = {
   description: "Airbnb clone",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
       <body className={inter.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal/>
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser}/>
         </ClientOnly>
         {children}
       </body>
