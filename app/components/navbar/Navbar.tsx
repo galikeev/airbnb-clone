2b32f6ce4c83e3c3c3db88544131e24aa0b39330
Ramil Galikeev <galikeevramil95@gmail.com>
2023-07-20 14:31:02 +0500

    added categories

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 import { SafeUser } from "@/app/types/type";
+import Categories from "@/app/components/navbar/Categories";
 
 interface NavbarProps {
   currentUser?: SafeUser | null;
@@ -23,6 +24,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   );
 };
